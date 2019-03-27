cfb101459f3881f0bad04fea74a006f2ea5eafaa
abiskup <andrija.biskup@gmail.com>
2019-03-27 21:00:43 +0100

    add package.json halls.js uniq.Halls.js ...

---
+++ server.js
@@ -0,0 +1,54 @@
+var hall = [];
+var hall1 = {
+ ID: 1,
+ name: "Velika sportska dvorana",
+ reservation: {
+   isReserved: false,
+   reservedFrom: 0,
+   reservedUntil: 0,
+ }
+}
+
+var hall2 = {
+ ID: 2,
+ name: "Dvorana Hrvatskog doma",
+ reservation: {
+   isReserved: true,
+   reservedFrom: new Date (2019, 08, 12),
+   reservedUntil: new Date (2019, 08, 15)
+ }
+}
+
+var hall3 = {
+ ID: 3,
+ name: "Dvorana Gradske knjižnice",
+ reservation: {
+   isReserved: false,
+   reservedFrom: 0,
+   reservedUntil: 0,
+ }
+}
+
+var hall4 = {
+ ID: 4,
+ name: "Dvorana TIC (Turistički dom)",
+ reservation: {
+   isReserved: true,
+   reservedFrom: new Date (2019, 04, 17),
+   reservedUntil: new Date (2019, 04, 22),
+ }
+}
+
+var hall5 = {
+ ID: 5,
+ name: "Konferencijske hall RCTP (Razvojni centar i tehnološki park)",
+ reservation: {
+   isReserved: false,
+   reservedFrom: 0,
+   reservedUntil: 0,
+ }
+}
+
+var reserv = [name]
+
+var x= ""
