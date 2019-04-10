26219ddfa65b8dadaa53f54c17bfa75c4a73daf7
abiskup <andrija.biskup@gmail.com>
2019-04-10 19:31:58 +0200

    change index.js

--- homework-12/index.js
+++ homework-12/index.js
@@ -50,7 +50,7 @@ var dvorana2 = {
    
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3013;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
